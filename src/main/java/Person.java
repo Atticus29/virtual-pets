3446f85fdf305c8b9210ed6e166553b7a48342d5
Christian Martinez and Mark Fisher <mark@>
2017-04-03 08:59:22 -0700

    add constructor, some properties, two getter methods and tests thereof for the person class, as well as set up the databases

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,21 @@
 import org.sql2o.*;
 
 public class Person {
+  private String name;
+  private String email;
 
-  public Person() {
 
+  public Person(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public String getEmail(){
+    return this.email;
   }
 
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -3,18 +3,30 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PersonTest {
+  private Person testPerson;
 
   @Before
   public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", null, null);
+    testPerson = new Person("Henry", "henry@henry.com");
+    // testPerson.save();
   }
 
-  @After
-  public void tearDown() {
-    try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
-      con.createQuery(sql).executeUpdate();
-    }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void person_instantiatesCorrectly_true(){
+    assertTrue(testPerson instanceof Person);
+  }
+
+  @Test
+  public void getName_personInstantiatesWithName_Henry(){
+    assertEquals("Henry", testPerson.getName());
+  }
+
+  @Test
+  public void getEmail_personInstantiatesWithEmail_String(){
+    assertEquals("henry@henry.com", testPerson.getEmail());
   }
 
 }
